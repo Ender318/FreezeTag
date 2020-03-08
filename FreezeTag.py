ce122b6c388d77c24ce6db938e3fee04aeea42e3
Ender318 <60166501+Ender318@users.noreply.github.com>
2020-03-08 17:44:29 -0500

    Update FreezeTag.py

--- FreezeTag.py
+++ FreezeTag.py
@@ -1,36 +1,57 @@
 import discord
-import random
 import time
-import asyncio
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='!')
 client = discord.Client()
 players = []
-inThisGuild = []
+tagged = []
+
+
+# starts the game timer
+@bot.command(name='startGame')
+async def start(ctx):
+    gameManager = discord.utils.get(ctx.guild.roles, name='Game Manager')
+    roles = ctx.message.author.roles
+    global time1
+    bool = False
+    while True:
+        for i in roles:
+            if i == gameManager:
+                time1 = time.time()
+                message = 'worked'
+                await ctx.send(message)
+                bool = True
+                break
+            else:
+                pass
+        if bool:
+            break
+        else:
+            message = 'Only the Game Manager can start the game.'
+            await ctx.send(message)
+
+    for i in players:
+        player = players[i]
+        it = discord.utils.get(ctx.guild.roles, name='It')
+        for role in player.roles:
+            if role == it:
+                players.remove(i)
+            else:
+                pass
+    players.sort()
+
 
 # enters a user into the game; assigns Tag Player Role
 @bot.command(name='joinGame')
 async def join(ctx):
-    user = ctx.message.author.id
-    guild = ctx.message.guild.id
-    inThisGuild.append(user)
-    players.append(user)
-    playersList = 'Players:', players
     player = ctx.message.author
+    players.append(player)
     role = discord.utils.get(ctx.guild.roles, name='Tag Player')
     await player.add_roles(role)
     response = 'You joined the game'
     await ctx.send(response)
 
-# returns the status of a user
-@bot.command(name='status')
-async def statux(ctx):
-    users = ctx.message.mentions
-    for i in users:
-        user = discord.utils.get(ctx.message.mentions)
-        status = user.status
-        await ctx.send(status)
 
 # freezes a user if the command user is it
 @bot.command(name='tag')
@@ -38,15 +59,34 @@ async def tag(ctx):
     roles = ctx.message.author.roles
     it = discord.utils.get(ctx.guild.roles, name='It')
     tagPlayer = discord.utils.get(ctx.guild.roles, name='Tag Player')
+
+    # checks if the tag is legal
     if tagPlayer in roles:
         if it in roles:
             player = discord.utils.get(ctx.message.mentions)
-            frozen = discord.utils.get(ctx.guild.roles, name='Frozen')
-            await player.add_roles(frozen)
+            if tagPlayer in player.roles:
+                if player.status == discord.Status.online:
+                    frozen = discord.utils.get(ctx.guild.roles, name='Frozen')
+                    await player.add_roles(frozen)
+                    tagged.append(player)
+                    tagged.sort()
+                    if players == tagged:
+                        await endGame(ctx)
+                    else:
+                        pass
+                else:
+                    message = 'You can only tag someone who is online!'
+                    await ctx.send(message)
+            else:
+                message = 'You can only tag someone who is playing the game!'
+                await ctx.send(message)
         else:
-            pass
+            message = 'You can only tag someone if you are it!'
+            await ctx.send(message)
     else:
-        pass
+        message = 'You must be playing the game to participate!'
+        await ctx.send(message)
+
 
 # unfreezes players if command user isn't it or frozen
 @bot.command(name='unfreeze')
@@ -68,12 +108,29 @@ async def unfreeze(ctx):
     else:
         pass
 
-# returns a list of players
-@bot.command(name='players')
-async def player(ctx):
-    for i in inThisGuild:
-        player = players[i].name
-        await ctx.send(player)
+
+# ends the game
+async def endGame(ctx):
+    global time2
+    time2 = time.time()
+    global gameTime
+    gameTime = time2 - time1
+    hours = gameTime // 3600
+    minutes = gameTime // 60
+    seconds = gameTime % 3600
+    message = 'The game time was ' + str(hours) + 'hours, ' + str(minutes) + 'minutes, and ' + str(seconds) + 'seconds.'
+    await ctx.send(message)
+
+
+# returns the status of a user
+@bot.command(name='status')
+async def statux(ctx):
+    users = ctx.message.mentions
+    for i in users:
+        user = discord.utils.get(ctx.message.mentions)
+        status = user.status
+        await ctx.send(status)
+
 
 # returns the latency of the bot
 @bot.command(name='ping')
@@ -81,12 +138,18 @@ async def ping(ctx):
     ping = bot.latency
     await ctx.send(ping)
 
+
 # returns the rules
 @bot.command(name='rules')
 async def rules(ctx):
-    message = '```Rules: \n1. Only tag people that are online\n2. test```'
+    message = '```Rules: \n1. Only tag people that are online\n2. Only tag people playing the game. ' \
+              'This runs on mentions so don\'t spam others\n3. Do not use an invisible status to hide: that is cheating\n' \
+              '4. Do not join the game late, it will mess up the times\n' \
+              '5. The time starts when an admin uses the start game command\n' \
+              '\n**HOW TO PLAY**\n' \
+              'This game is tournament style. Everyone playing takes turns being \"it\". After everyone has taken ' \
+              'their turn, the person with the fastest time to end the game is the winner.```'
     await ctx.send(message)
-
 
 
 bot.run('TOKEN')
